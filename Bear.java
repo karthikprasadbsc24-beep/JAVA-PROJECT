d32f4c4f3d3e9005c2856b99ef38920b4edd7fc4
karthikprasadbsc24-beep <karthikprasadbsc24@rvu.edu.in>
2025-11-04 10:24:50 +0530

    Enhance Bear constructor with attribute logging
    Added constructor logging for Bear class attributes.

--- Bear.java
+++ Bear.java
@@ -1,5 +1,18 @@
 public class Bear extends Monster {
     public Bear() {
         super(3, "Ayı", 7, 20, 12);
+        System.out.println("A Bear has been created!");
+        System.out.println("ID: 3");
+        System.out.println("Name: Ayı");
+        System.out.println("Damage: 7");
+        System.out.println("Health: 20");
+        System.out.println("Award: 12");
+    }
+
+    public static void main(String[] args) {
+        
+        new Bear();
     }
 }
+    
+
